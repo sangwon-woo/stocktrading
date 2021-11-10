008bd81e3448def8b05d14b00b6813f03a5bd9da
SangwonWoo <riddlewoo713@gmail.com>
2021-11-10 21:32:18 +0900

    update setting.py

--- setting.py
+++ setting.py
@@ -15,7 +15,17 @@ CSV_DAILY_CHECKLIST = PWD + '\\data\\daily_checklist.csv'
 CSV_LASTEST_CHECKLIST = PWD + f'\\data\\past_check\\daily_checklist_{TODAY}.csv'
 
 CHECKLIST_DTYPE ={
-
+    '시장명' : 'category',
+    '종목명' : 'category',
+    '종목코드' : 'category',
+    '감리구분' : 'category',
+    '체크최종수정일' : 'category',
+    '일봉관리여부' : bool,
+    '일봉최종수정일' : 'category',
+    '일봉최초일' : 'category',
+    '일봉최말일' : 'category',
+    '분봉관리여부' : bool,
+    '분봉최종수정일' : 'category'
 }
 
 TRADEDATA_DTYPE = {
