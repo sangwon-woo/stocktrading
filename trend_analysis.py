464dcc4350b942e4eea50f9cce0013dd47a16a65
SangwonWoo <riddlewoo713@gmail.com>
2021-11-11 18:20:53 +0900

    update trend_analysis.py

--- trend_analysis.py
+++ trend_analysis.py
@@ -9,3 +9,88 @@
 아니면 1개월부터 볼까. 잘 모르겠다. 
 요즘 시장은 2주
 '''
+
+import pandas as pd
+from setting import *
+
+def trend_analysis(timespan, code):
+    nrows = TIMESPAN[timespan] # N3
+    if code in kospi_code_list_we_have:
+        
+        df = pd.read_csv(DIR_KOSPI_DAILY + f'\\{code}.csv', 
+                         encoding='utf-8', 
+                         dtype=TRADEDATA_DTYPE, 
+                         parse_dates=['날짜'])
+    else:
+        df = pd.read_csv(DIR_KOSDAQ_DAILY + f'\\{code}.csv', 
+                         encoding='utf-8', 
+                         dtype=TRADEDATA_DTYPE, 
+                         parse_dates=['날짜'])
+    if df.shape[0] < nrows:
+        return None
+    
+    if not df.loc[0, '거래량']:
+        return None
+    
+    df = df.iloc[:nrows, :]
+    df = df.sort_values(by=['날짜']).reset_index(drop=True)
+    df.reset_index(inplace=True)
+
+    df['평균매매가'] = (df[['시가', '종가']].sum(axis=1) / 2 + df[['시가', '종가', '저가', '고가']].sum(axis=1))/5
+    standard_price = df.loc[0, '평균매매가']
+
+    df['변환주가'] = (df['평균매매가'] / standard_price * 10000)
+    max_price = df['변환주가'].max()                                                      # P2
+    min_price = df['변환주가'].min()                                                      # P3
+    xi = df.loc[df['변환주가'] == max_price, 'index'].values[0]
+    ni = df.loc[df['변환주가'] == min_price, 'index'].values[0]
+    max_index = 1 if df.loc[0, '변환주가'] == max_price else xi                          # L2
+    min_index = 1 if df.loc[0, '변환주가'] == min_price else ni                          # M2
+    average_price_move = (max_price - min_price) / max_index                             # R3
+    first_price = 10000                                                                  # T2
+    last_price = df['변환주가'].values[-1]                                                # T3
+
+    X_of_open_close = (last_price - first_price) / nrows                                  # T12 = (T3-T2) / N3
+    intercept_of_open_close = 10000                                                       # V12
+    Y_of_open_close = lambda x: X_of_open_close * x + intercept_of_open_close             # 
+
+    X_of_high_low = (max_price - min_price) / (max_index - min_index)                     # T16 = (P2-P3) / (L2-M2)
+    intercept_of_high_low = max_price - (max_index * X_of_high_low)                       # V16 = P2 - (L2*T16)
+    Y_of_high_low = lambda x: X_of_high_low * x + intercept_of_high_low
+
+    _1 = (1 + X_of_open_close ** 2) ** (1 / 2)                                            # W13
+    _2 = (1 + X_of_high_low ** 2) ** (1 / 2)                                              # W17
+    _3 = _1 / _2                                                                          # W15
+    _4 = first_price - intercept_of_high_low                                              # U23 = T2 - V16
+    _5 = X_of_high_low - X_of_open_close                                                  # S23 = T16 - T12
+    _6 = _4 / _5                                                                          # T24 = U23 / S23
+    _7 = X_of_high_low * _6 + intercept_of_high_low                                       # U24
+
+    X_of_cross_line = (X_of_open_close + (X_of_high_low * _3)) / (1 + _3)                 # T22
+    intercept_of_cross_line = _7 - (_6 * X_of_cross_line)                                 # V22 = U24 - (T24*T22)
+    Y_of_cross_line = lambda x: X_of_cross_line * x + intercept_of_cross_line
+
+
+
+    x_axis_one_scale = (max_price - min_price) / (nrows -1) # AI10
+    x_data_count = nrows -1 # AI11
+    max_min_height = max_price - min_price # AI12
+    best_trend_energy = x_data_count * max_min_height / 2 # AI13
+    open_close_trend_energy = (last_price - first_price) * x_data_count / 2 # AI14
+    high_low_trend_energy = max_min_height * (max_index - min_index) / 2 # AI15
+    open_close_trend_energy_pct = open_close_trend_energy / best_trend_energy * 100# AJ14
+    high_low_trend_energy_pct = high_low_trend_energy / best_trend_energy * 100# AJ15
+    up_trend_power = (open_close_trend_energy_pct + high_low_trend_energy_pct) / 2 # T7
+
+
+    df['시종에너지'] = open_close_trend_energy / 10000
+    df['고저에너지'] = high_low_trend_energy / 10000
+    df['에너지소계'] = df['시종에너지'] + df['고저에너지']
+    cross_line_gap = (df['변환주가'] - (df['index'].apply(Y_of_cross_line))).abs()
+    df['중심선괴리합'] = cross_line_gap.sum()
+    df['중심선괴리평균'] = cross_line_gap.mean()
+    df['괴리도'] = cross_line_gap.mean() / ((first_price + last_price) / 2) * 100
+    df['기간상승/하락률'] = ((last_price / first_price) - 1) *100
+    df['상승추세강도'] = up_trend_power
+    
+    return df.iloc[-1, :]
