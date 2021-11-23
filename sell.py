680a9237f8fd319f1be70f3bb78ed194b5575574
SangwonWoo <riddlewoo713@gmail.com>
2021-11-23 13:58:59 +0900

    Modify sell.py

--- sell.py
+++ sell.py
@@ -5,3 +5,21 @@
     익절
 손절 및 익절 모두 자산관리가 필요하다. 
 '''
+
+from pykiwoom.kiwoom import Kiwoom
+import pandas as pd
+import numpy as np
+import time
+
+
+
+
+
+if __name__ == "__main__":
+    kiwoom = Kiwoom()
+    kiwoom.CommConnect(block=True)
+
+    accounts = kiwoom.GetLoginInfo('ACCNO')
+    account = accounts[0]
+
+    ret = kiwoom.SendOrder('시장가매수', '0101', account, 1, '000900', 100, 0, '03', '') # 시장가 등록. 
